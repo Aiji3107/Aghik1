8aa3307675f99e8e3c30d5c849f3146b586cf312
Aiji3107 <96954633+Aiji3107@users.noreply.github.com>
2022-01-02 15:11:44 +0700

    Update tugastpt.cpp

--- tugastpt.cpp
+++ tugastpt.cpp
@@ -1,5 +1,5 @@
-# Aghik1
-//Code Quiz
+#aghik
+//code quiz
 
 #include <iostream>
 #include <conio.h>
@@ -10,15 +10,23 @@ using namespace std;
 
 //variabel
 int point = 0;
-char answer;
+int ppoint;
+char chs1, chs2, chs3, chs4, chs5, ans1, ans2, ans3, ans4;
 
+//tpoint()
+//{
+// int point = point + ppoint;
+//return point;
+//}
 int main()
 {
-    char chs1, chs2, chs3, chs4, ans1, ans2, ans3;
+
+home:
+    point = 0;
     cout << "=====================================================\n";
-    cout << "                    KUIS KOMPUTER                    \n";
-    cout << "                 Press Enter to Start                        \n";
-    cout << "Mau Langsung ke Pertanyaan Pertama ? (y/n)";
+    cout << "                    KUIS KOMPUTER                    \n\n\n\n\n";
+    cout << "                                                     \n";
+    cout << "            Want to Start the Quiz? (y/n) ";
     cin >> chs1;
     switch (chs1)
     {
@@ -34,62 +42,73 @@ int main()
         exit(0);
     }
 soal1: //soal 1
+
     system("cls");
     cout << "Pertanyaan 1 :\n\n";
-    cout << " soal pertama coba dulu sudah di perbaiki" << endl;
-    cout << "A. jawaban 1\n";
-    cout << "B. jawaban 1\n";
-    cout << "C. jawaban 1\n";
-    cout << "D. jawaban 1\n";
+    cout << " Pekerjaan Seorang Programer adalah..." << endl;
+    cout << "A. Memancing Ikan\n";
+    cout << "B. Memasak\n";
+    cout << "C. Coding\n\n";
+    cout << "Jawab  :";
     cin >> ans1;
     switch (ans1)
     {
     case 'A':
     case 'a':
-        cout << "jawaban Anda Salah. Silahkan ulang dari awal ! (y/n)\n";
+        cout << "jawaban Anda Salah ! Lanjut soal berikutnya ? (y/n)\n";
         cin >> chs2;
         {
             if ((chs2 == 'y') || (chs2 == 'Y'))
             {
-                goto soal1;
+                ppoint = 0;
+                //tpoint();
+                point = point + ppoint;
+                goto soal2;
             }
             else if ((chs2 == 'n') || (chs2 == 'N'))
             {
-                exit(0);
+                goto home;
+                ;
             }
         }
         break;
     case 'B':
     case 'b':
-        cout << "maaf, jawaban masih belum tepat. Silahkan ulang dari awal ! (y/n)\n";
+        cout << "jawaban Anda Salah ! Lanjut soal berikutnya ? (y/n)\n";
         cin >> chs2;
         {
             if ((chs2 == 'y') || (chs2 == 'Y'))
             {
-                goto soal1;
+                ////ppoint = 0;
+                ////tpoint();
+                goto soal2;
             }
             else if ((chs2 == 'n') || (chs2 == 'N'))
             {
-                exit(0);
+                ppoint = 0;
+                goto home;
             }
         }
         break;
     case 'C':
     case 'c':
-        cout << "Selamat, jawaban kamu benar. Mau lanjut ke Pertanyaan berikutnya ? (y/n)\n";
+        cout << "Selamat, jawaban kamu benar. Lanjut ke soal berikutnya ? (y/n)\n";
         cin >> chs2;
         {
             if ((chs2 == 'y') || (chs2 == 'Y'))
             {
-                point = point + 25;
+                ppoint = 25;
+                //tpoint();
+                point = point + ppoint;
                 goto soal2;
             }
             else if ((chs2 == 'n') || (chs2 == 'N'))
             {
-                exit(0);
+                goto home;
             }
         }
         break;
+
     default:
         exit(0);
     }
@@ -97,57 +116,64 @@ soal1: //soal 1
 soal2: //soal 2
     system("CLS");
     cout << "Pertanyaan 2 :\n\n";
-    cout << "Ada berapa macam-macam Array ?\n";
-    cout << "A. jawaban 2\n";
-    cout << "B. jawaban 2\n";
-    cout << "C. jawaban 2\n";
-    cout << "D. jawaban 2\n";
+    cout << "Software yang digunakan programer untuk menulis dan mengcompile adalah...\n";
+    cout << "A. Visual Studio Code\n";
+    cout << "B. Adobe Premier Pro\n";
+    cout << "C. Steam\n";
+    cout << "D. Chrome\n\n";
+    cout << "Jawab  :";
     cin >> ans2;
     switch (ans2)
     {
     case 'A':
     case 'a':
-        cout << "Selamat, jawaban kamu benar. Mau lanjut ke pertanyaan berikutnya ? (y/n)\n";
+        cout << "Selamat, jawaban Kamu benar. Lanjut ke soal berikutnya ? (y/n)\n";
         cin >> chs3;
         {
             if ((chs3 == 'y') || (chs3 == 'Y'))
             {
-                point = point + 25;
+                ppoint = 25;
+                point = point + ppoint;
+                //tpoint();
                 goto soal3;
             }
             else if ((chs3 == 'n') || (chs3 == 'N'))
             {
-                exit(0);
+                goto home;
             }
         }
         break;
     case 'B':
     case 'b':
-        cout << "maaf, jawaban masih belum tepat. Silahkan ulang dari awal ! (y/n)\n";
+        cout << "jawaban Anda Salah ! Lanjut soal berikutnya ? (y/n)\n";
         cin >> chs3;
         {
             if ((chs3 == 'y') || (chs3 == 'Y'))
             {
-                goto soal1;
+                //ppoint = 0;
+                //tpoint();
+                goto soal3;
             }
             else if ((chs3 == 'n') || (chs3 == 'N'))
             {
-                exit(0);
+                goto home;
             }
         }
         break;
     case 'C':
     case 'c':
-        cout << "maaf, jawaban masih belum tepat. Silahkan ulang dari awal ! (y/n)\n";
+        cout << "jawaban Anda Salah ! Lanjut soal berikutnya ? (y/n)\n";
         cin >> chs3;
         {
             if ((chs3 == 'y') || (chs3 == 'Y'))
             {
-                goto soal1;
+                //ppoint = 0;
+                //tpoint();
+                goto soal3;
             }
             else if ((chs3 == 'n') || (chs3 == 'N'))
             {
-                exit(0);
+                goto home;
             }
         }
         break;
@@ -157,8 +183,8 @@ soal2: //soal 2
 
 soal3: //soal 3
     system("CLS");
-    cout << "Pertanyaan 3 :\n\n";
-    cout << "soal nomber three\n";
+    cout << "Pertanyaan tiga\n\n";
+    cout << "Nama lain dari flowchart adalah...\n";
     cout << "A. jawaban 3\n";
     cout << "B. jawaban 3\n";
     cout << "C. jawaban 3\n";
@@ -168,47 +194,120 @@ soal3: //soal 3
     {
     case 'A':
     case 'a':
-        cout << "maaf, jawaban masih belum tepat. Silahkan ulang dari awal ! (y/n)\n";
+        cout << "jawaban Anda Salah ! Lanjut hasil nilai ? (y/n)\n";
         cin >> chs4;
         {
             if ((chs4 == 'y') || (chs4 == 'Y'))
             {
-                goto soal1;
+                //ppoint = 0;
+                //tpoint();
+                goto hasil;
             }
             else if ((chs4 == 'n') || (chs4 == 'N'))
             {
-                exit(0);
+                goto home;
             }
         }
         break;
     case 'B':
     case 'b':
-        point = point + 25;
-        goto hasil;
+        cout << "Selamat, jawaban Kamu benar. Lanjut ke soal berikutnya ? (y/n)\n";
+        cin >> chs3;
+        {
+            if ((chs3 == 'y') || (chs3 == 'Y'))
+            {
+                ppoint = 25;
+                point = point + ppoint;
+                //tpoint();
+                goto soal4;
+            }
+            else if ((chs3 == 'n') || (chs3 == 'N'))
+            {
+                goto home;
+            }
+        }
         break;
-    default:
-        exit(0);
+
     case 'C':
     case 'c':
-        cout << "maaf, jawaban masih belum tepat. Silahkan ulang dari awal ! (y/n)\n";
+        cout << "jawaban Anda Salah ! Lanjut hasil nilai ? (y/n)\n";
         cin >> chs4;
         {
             if ((chs4 == 'y') || (chs4 == 'Y'))
             {
-                goto soal1;
+                ppoint = 0;
+                //tpoint();
+                point = point + ppoint;
+                goto hasil;
             }
             else if ((chs4 == 'n') || (chs4 == 'N'))
             {
-                exit(0);
+                goto home;
             }
         }
         break;
+    }
+soal4: //soal 4
+    system("CLS");
+    cout << "Pertanyaan 4\n\n";
+    cout << "Yang dimaksud dengan tipe data Double adalah ?\n";
+    cout << "A. Bilangan Ganjil\n";
+    cout << "B. Bilangan Desimal\n";
+    cout << "C. Bilangan bulat\n";
+    cin >> ans4;
+    switch (ans4)
+    {
+    case 'A':
+    case 'a':
+        cout << "jawaban Anda Salah ! Lanjut hasil nilai ? (y/n)\n";
+        cin >> chs5;
+        {
+            if ((chs5 == 'y') || (chs5 == 'Y'))
+            {
+                //ppoint = 0;
+                //tpoint();
+                goto hasil;
+            }
+            else if ((chs5 == 'n') || (chs5 == 'N'))
+            {
+                goto home;
+            }
+        }
+        break;
+    case 'B':
+    case 'b':
+        ppoint = 25;
+        point = point + ppoint;
+        //tpoint();
+        goto hasil;
+        break;
+
+    case 'C':
+    case 'c':
+        cout << "jawaban Anda Salah ! Lanjut hasil nilai ? (y/n)\n";
+        cin >> chs4;
+        {
+            if ((chs4 == 'y') || (chs4 == 'Y'))
+            {
+                ppoint = 0;
+                //tpoint();
+                point = point + ppoint;
+                goto hasil;
+            }
+            else if ((chs4 == 'n') || (chs4 == 'N'))
+            {
+                goto home;
+            }
+        }
+        break;
+    default:
+        exit(0);
     }
 hasil: //hasil
     system("cls");
     system("COLOR 1E");
     cout << "Nilai Kamu Adalah " << point << endl;
-    cout << "Selamat, kamu telah menjawab semua pertanyaan dalam game quiz ini\n";
-    cout << "Terima kasih sudah bermain";
+    cout << "Selamat, kamu telah menjawab pertanyaan dalam game quiz ini\n";
+    cout << "Terima kasih sudah bermain !";
     getch();
 }
